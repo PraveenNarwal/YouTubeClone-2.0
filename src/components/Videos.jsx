d4f67f591857040ed095cd69ab5bdf4bebe05f05
PraveenNarwal <praveennarwal5@gmail.com>
2022-08-27 01:17:11 +0530

    video cloumn added

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import { Stack, Box }                       from "@mui/material";
+import React                                from "react";
+import { ChannelCard, Loader, VideoCard }   from "./";
 
-const Videos = () => {
+const Videos = ({ videos, direction }) => {
+    if(!videos?.length) return <Loader />;
+
   return (
-    <div>Videos</div>
+    <Stack direction={direction || "row"} flexWrap="wrap" justifyContent="start" alignItems="start" gap={2}>
+      {videos.map((item, idx) => (
+        <Box key={idx}>
+          {item.id.videoId && <VideoCard video={item} /> }
+          {item.id.channelId && <ChannelCard channelDetail={item} />}
+        </Box>
+      ))}
+    </Stack>
   )
 }
 
