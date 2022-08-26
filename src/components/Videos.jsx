9aaced39d8ac38f9c6c092d376a4b64ff6615bdf
PraveenNarwal <praveennarwal5@gmail.com>
2022-08-27 01:36:33 +0530

    API fetch from RapidAPI

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -3,6 +3,7 @@ import React                                from "react";
 import { ChannelCard, Loader, VideoCard }   from "./";
 
 const Videos = ({ videos, direction }) => {
+    console.log(videos)
     if(!videos?.length) return <Loader />;
 
   return (
